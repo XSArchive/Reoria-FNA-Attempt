4dfef17ce8c2ea6fc88550bd8c133467178e3cc0
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-07-18 23:01:16 -0400

    Added FNA.Runtimes project.

---
+++ source/FNA.Runtimes/RuntimeInformation.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FNA.Runtimes
+{
+    public static class RuntimeInformation
+    {
+        public static readonly DateTime Updated = new DateTime(2023, 07, 18, 22, 47, 0);
+    }
+}
